2c55de071058ea643e4b31f239fb0c430270953e
Roberto Arcomano <bertolinux@gmail.com>
2022-10-01 16:31:51 +0200

    Changed pod to deploy

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 # C. Constants
 NS=devops
-JENKINS_IMAGE=jenkins/jenkins:lts
-JENKINS_POD=jenkins
+JENKINS_DEPLOY_FILE=jenkins-deploy.yaml
+JENKINS_POD=""
 JENKINS_SVC_FILE=jenkins-svc.yaml
 PASSWORD_FILE=/var/jenkins_home/secrets/initialAdminPassword
 now="--grace-period=0 --force"
@@ -14,15 +14,17 @@ kubectl delete ns $NS $now
 # 1. Create ns
 kubectl create ns $NS
 
-# 2. Create pod jenkins
-kubectl run -n $NS $JENKINS_POD --image $JENKINS_IMAGE
+# 2. Create deploy jenkins
+kubectl replace -f $JENKINS_DEPLOY_FILE --force
 
 # 3. Create service jenkins
 kubectl replace -f $JENKINS_SVC_FILE --force
 
 # 4. Wait until pod is ready
+JENKINS_POD=$(kubectl -n $NS get pod --no-headers | grep -i jenkins | awk '{print $1}')
+echo "JENKINS_POD: " $JENKINS_POD
 until kubectl -n $NS get pod $JENKINS_POD|grep Running; do sleep 1; done
-until kubectl -n $NS logs $JENKINS_POD |grep initialAdminPassword; do sleep 1; done
+until kubectl -n $NS logs $JENKINS_POD |grep "Jenkins is fully up and running"; do sleep 1; done
 
 # 5. Show password
 echo -e "Jenkins password: "
